37d7df0c0913ef55a6d1652dc91e7f824620ba40
yuqn <2572655497@qq.com>
2024-05-30 19:11:05 +0800

    封装redis工具类

--- yuqn-boot-base-core/src/main/java/com/yuqn/config/RedisConfig.java
+++ yuqn-boot-base-core/src/main/java/com/yuqn/config/RedisConfig.java
@@ -1,13 +1,10 @@
 package com.yuqn.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.*;
 
 /**
  * @author: yuqn
@@ -19,7 +16,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
     @Bean
-    @ConditionalOnSingleCandidate(RedisConnectionFactory.class)
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
@@ -30,9 +26,11 @@ public class RedisConfig {
         // 设置hash key的序列化器为StringRedisSerializer
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         // 设置value的序列化器为JdkSerializationRedisSerializer
-        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        // redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer()); // 默认序列化方式
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
         // 设置hash value的序列化器为JdkSerializationRedisSerializer
-        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+        // redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer()); // 默认序列化方式
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
         // 初始化RedisTemplate
         redisTemplate.afterPropertiesSet();
         // 返回配置好的RedisTemplate
--- yuqn-boot-model-demo/src/main/java/com/yuqn/controller/redis/RedisDemo.java
+++ yuqn-boot-model-demo/src/main/java/com/yuqn/controller/redis/RedisDemo.java
@@ -1,9 +1,10 @@
 package com.yuqn.controller.redis;
 
+import com.yuqn.entity.RedisObj;
+import com.yuqn.utils.RedisCache;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,20 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "demo模块")
 public class RedisDemo {
 
-    private RedisTemplate redisTemplate;
-
+    private RedisCache redisCache;
     @Autowired
-    public void setRedisTemplate(RedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
+    public void setRedisCache(RedisCache redisCache) {
+        this.redisCache = redisCache;
     }
 
     @GetMapping("/set")
     @Operation(summary = "保存redis")
     public String setmes(){
+        RedisObj redisObj = new RedisObj("01","yuqn",23);
         try {
-            redisTemplate.opsForValue().set("one","yuqn");
-            redisTemplate.opsForValue().set("two","24");
+            redisCache.setCacheObject("name","余其楠");
+            redisCache.setCacheObject("cacheObj",redisObj);
         }catch (Exception e){
+            System.out.println(e.getMessage());
             System.out.println("保存失败");
             return "保存失败";
         }
@@ -43,10 +45,10 @@ public class RedisDemo {
     @Operation(summary = "获取redis")
     public String getmes(){
         try {
-            String mes = redisTemplate.opsForValue().get("one").toString();
-            String age = redisTemplate.opsForValue().get("two").toString();
-            System.out.println(mes);
-            System.out.println(age);
+            String name = redisCache.getCacheObject("name");
+            RedisObj cacheObj = redisCache.getCacheObject("cacheObj");
+            System.out.println("name = " + name);
+            System.out.println("cacheObj = " + cacheObj);
         }catch (Exception e){
             System.out.println("保存失败");
             return "获取失败";
